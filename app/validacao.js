d5d3d2b0e271450e6d7bb2fbb1b390c720a4222f
LeninMatos <leninmatos@gmail.com>
2023-03-09 12:19:48 -0300

    criando o botão jogar novamente

--- app/validacao.js
+++ app/validacao.js
@@ -13,11 +13,12 @@ function verificaSeOChutePossuiUmValorValido(params) {
         document.body.innerHTML = `
         <h2>Você Acertou!</h2>
         <h3>O número secreto era ${numeroScereto}</h3>
+        <button id="jogar-novamente" class="btn-jogar-novamente">Jogar Novamente</button>
         `
     } else if (numero > numeroScereto) {
         elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`
-    } else { 
-        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>` 
+    } else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`
     }
 }
 
@@ -28,3 +29,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOPermitido(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', evento => {
+    if (evento.target.id == 'jogar-novamente') {
+        window.location.reload();
+    }
+})
