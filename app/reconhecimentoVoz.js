95e208094153c00428ba7515189782303115dc7c
LeninMatos <leninmatos@gmail.com>
2023-03-09 10:50:32 -0300

    Exibindo mensagem falada no console

--- app/reconhecimentoVoz.js
+++ app/reconhecimentoVoz.js
@@ -5,3 +5,9 @@ const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br';
 
 recognition.start();
+
+recognition.addEventListener('result',onSpeak);
+
+function onSpeak(evento) {
+    console.log(evento.results[0][0].transcript)
+}
